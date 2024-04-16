4cc6e0675df488664c22f75dc64bef0d3fcc8f95
anwar-1999 <137158902+anwar-1999@users.noreply.github.com>
2024-04-16 07:43:55 +0530

    Add files via upload

---
+++ src/app/Utils/SessionStorage.ts
@@ -0,0 +1,22 @@
+export const setAllUserInfo = (user: any) => {
+    let stringifyUser = JSON.stringify(user);
+    localStorage.setItem("allUser", stringifyUser);
+}
+export const getAllUserInfo = () => {
+    if (typeof window !== 'undefined') {
+        let stringifyUser: any = localStorage.getItem("allUser");
+        return JSON.parse(stringifyUser);
+    }
+    return null; // Or handle the case where localStorage is not available
+}
+export const setUserInfo = (user: any) => {
+    let stringifyUser = JSON.stringify(user);
+    localStorage.setItem("user", stringifyUser);
+}
+export const getUserInfo = () => {
+    if (typeof window !== 'undefined') {
+        let stringifyUser: any = localStorage.getItem("user");
+        return JSON.parse(stringifyUser);
+    }
+    return null; // Or handle the case where localStorage is not available
+}
